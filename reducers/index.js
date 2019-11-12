585353b6f986a6d91043637f27f4fc9fb4418a28
aramirezdecruz3148 <leigh3148@gmail.com>
2019-11-12 12:53:47 -0800

    created spellReducer with passing tests

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import health from './healthReducer';
+import spells from './spellReducer';
 
 export default combineReducers({
-  health
+  health,
+  spells
 });
