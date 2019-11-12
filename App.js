085b9355c8b60ac82c6fe02c284511e59a50a59f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-11-11 18:33:38 -0800

    created a Game.js component to house all game components, plugged it into App, then wrapped App in Prodicer and brought in store

--- App.js
+++ App.js
@@ -1,51 +1,16 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import GameRound from './components/GameRound';
-import OpponentStats from './components/OpponentStats';
-import PlayerStats from './components/PlayerStats';
-import PlayerSpells from './components/PlayerSpells';
-import OutcomeModal from './components/OutcomeModal';
+import { Provider } from 'react-redux';
+import Game from './Game';
+import store from './store';
 
 export default function App() {
   return (
-    <>
-    <View style={styles.container}>
-      <GameRound round={3} />
-      <OpponentStats 
-      name="Draco Malfoy" 
-      image="https://vignette.wikia.nocookie.net/harrypotter/images/7/7e/Draco_Malfoy_TDH.png/revision/latest/scale-to-width-down/350?cb=20180116013508" 
-      health={10} />
-      <PlayerStats health={8} />
-      <PlayerSpells spellArray={[
-        {
-          spell: "Anteoculatia",
-          type: "Hex",
-          effect: "turns opponent's hair into antlers",
-          damage: -1,
-          outcome: `You were hit with the Anteoculatia hex, which caused
-          giant antlers to sprout on your head and took 1 damage point from
-          your health`
-        },
-        {
-          spell: "Avis",
-          type: "Spell",
-          effect: "launches birds at your opponent",
-          damage: -2,
-          outcome: `You were hit with the Avis spell, which caused birds to swarm
-          and attack you and took 2 damage points from your health`
-        },
-        {
-          spell: "Babbling Curse ",
-          type: "Curse",
-          effect: "makes opponent endlessly babble",
-          damage: -1,
-          outcome: `You were hit with the Babbling curse, which is caused you to 
-          endlessly babble none sense and took 2 damage points from your health`
-        }
-      ]} />
-      <OutcomeModal />
-    </View>
-    </>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Game />
+      </View>
+    </Provider>
   );
 }
 
