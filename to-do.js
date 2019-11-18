e23c239f4fb8f3ef0c704c9f6ef8571eed92c50e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-11-18 14:51:28 -0800

    added to my to-do notes and sketched out functions for getting villian, spells and spell from game-data

--- to-do.js
+++ to-do.js
@@ -1,15 +1,11 @@
 /* 
 1. Double check that components have the initial necessary props
-2. Create a component for game start button, will eventually be
-a modal that will start/end the game, but for simplicity just make
-it a button for now
-3. Need to create a duel button
-4. Create functions necessary to retrieve data (I think they should live in container for service folder):
+2. Create functions necessary to retrieve data (I think they should live in container for service folder):
   a. Function to get array of 5 random spells from data
   b. Function to get one random spell
   c. Function to get one random villian
-5. Start with hooking up functionality off of game start button
-6. Then start with the next round of functionality from the duel
+3. Start with hooking up functionality off of game start button
+4. Then start with the next round of functionality from the duel
 button
   a. Need to figure out how I want to tackle which spell is selected
     whether that will be stored in React state or Redux. I think it 
@@ -17,5 +13,37 @@ button
     to the spell objects: isSelected
       - Will need an action to update the isSelected property
       - Will need an action to remove the selected spell
-7. Will also need to make a component to hold all components that need to g
+5. Will also need to make a component to hold all components that need to go in the StartGame connect container
+*/
+
+/*
+//function for getting 5 random spells
+//will need to import spell array
+
+function fiveRandomSpells(spellArray) {
+  let spells = [];
+  for (var i = 0; i < 5; i++) {
+    const index = spellArray[Math.floor(Math.random() * 19)];
+    spells.push(index);
+  }
+  return spells
+}
+*/
+
+/*
+//function for getting 1 random spells
+//will need to import spell array
+
+function oneRandomSpells(spellArray) {
+  return spellArray[Math.floor(Math.random() * 19)];
+}
+*/
+
+/*
+//function for getting 1 random villian
+//will need to import villian array
+
+function oneRandomSpells(villianArray) {
+  return villianArray[Math.floor(Math.random() * 9)];
+}
 */
